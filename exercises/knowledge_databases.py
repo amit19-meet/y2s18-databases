837e8d185283bae181545aad94f9a88e7d55fd56
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 12:14:53 +0300

    databases2

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,20 +8,47 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(chosen_topic, title, rating):
+    knowledge_object = Knowledge(
+        chosen_topic=chosen_topic,
+        title=title,
+        rating=rating)
+    session.add(knowledge_object)
+    session.commit()
+topic1= add_article("swing dancing", "charleston", 8)
+print(topic1)
+
+
+    
 
 def query_all_articles():
-	pass
+    articles = session.query(Knowledge).all()
+    return articles
 
-def query_article_by_topic():
-	pass
+
+x= query_all_articles()
+print(x)
+
+
+
+def query_article_by_topic(chosen_topic):
+    art = session.query(Knowledge).filter_by(chosen_topic=chosen_topic).first()
+    return art
+print(query_article_by_topic("swing dancing"))
+
+def query_article_by_rating(rating):
+    threshold= input("rating")
+    rat = session.query(Knowledge).filter_by(rating<threshold).first()
+    return rat
+
+
+    
 
 def delete_article_by_topic():
-	pass
+    pass
 
 def delete_all_articles():
-	pass
+    pass
 
 def edit_article_rating():
-	pass
+    pass
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -7,15 +7,14 @@ Base = declarative_base()
 
 class Knowledge(Base):
 	__tablename__= "Knowledge"
-	student_number = Column(Integer, primary_key=True)
+	article_id = Column(Integer, primary_key = True)
 	chosen_topic = Column(String)
 	title= Column(String)
 	rating= Column(Integer)
 
 	def __repr__(self):
-       return ("Topic: {}\n"
-               "The title of the article: {}\n"
-               "Rating: {}\n").format(self.chosen_topic, self.title, self.rating)
+			return ("If you want to learn about {}\n, you should look at the Wikipedia article called {}\n."
+					"We gave this article a rating of {}\n out of 10").format(self.chosen_topic, self.title, self.rating)
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -25,5 +24,5 @@ class Knowledge(Base):
 	# an integer, representing your rating of the article.
 
 student1= Knowledge(chosen_topic= "swing dancing", title= "Charleston", rating= 8)
-print("If you want to learn about"+ student1.chosen_topic()+", you should look at the Wikipedia article called rainbow.
-We gave this article a rating of 9 out of 10!)
+print(student1) 
+
